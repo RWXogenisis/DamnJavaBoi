4ddf3dc5c2ad28a464ac7f9d43ab978320da30a1
RWXogenisis <65536776+RWXogenisis@users.noreply.github.com>
2022-12-23 16:00:13 +0530

    Update stackboi.java

--- stackboi.java
+++ stackboi.java
@@ -13,7 +13,7 @@ public class Main{
         System.out.println("Pushing the new element: " + stack.push("Boi"));
         System.out.println("Stack: " + stack);
         System.out.println("Is Boi there: " + stack.search("Boi"));
-        //returns 1 as it is indexed from left to right
+        //returns 1 as it is indexed from right to left
         ListIterator<String> l1 = stack.listIterator(0);
         while(l1.hasNext()){
             System.out.println(l1.next());
